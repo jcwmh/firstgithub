bd31dcac3ac11d7c92b999c7bc6ec58f8cf157c5
jcwmh <56943113+jcwmh@users.noreply.github.com>
2019-11-01 14:35:21 +0800

    Update ios_201741030122金唱.py

--- ios_201741030122金唱.py
+++ ios_201741030122金唱.py
@@ -5,46 +5,43 @@ import multiprocessing as mp
 import time
 
 t1=time.time()
-r = requests.get('https://car.autohome.com.cn/price/list-0-3-0-0-0-0-0-0-0-0-0-0-0-0-0-1.html')
+r = requests.get('https://b.faloo.com/y/0/0/0/4/2/0/1.html')
 
-c=r.text
+n=r.text
 
-soup=BeautifulSoup(c,'html.parser')
-content = soup.find_all('div',{'class':'list-cont'})
-print(content)
-page_div = soup.find('div',{'class':'page'})
-page = page_div.find_all('a')[-2].text
-cars= []
+soup=BeautifulSoup(n,'html.parser')
+#content = soup.find_all('div',{'class':'list-cont'})
 
-urls = ['https://car.autohome.com.cn/price/list-0-3-0-0-0-0-0-0-0-0-0-0-0-0-0-'+str(i)+'.html' for i in range(1,11)]
+page_div = soup.find('div',{'class':'l_page'})
+#print(page_div)
+page=page_div.find_all('a')[-2].text
+novels=[]
+
+urls = ['https://b.faloo.com/y/0/0/0/4/2/0/'+str(i)+'.html' for i in range(1,31)]
 
 def crawl_page(url):
 
     p_r = requests.get(url)
-    p_c=p_r.text
-    p_soup=BeautifulSoup(p_c,'html.parser')
-    p_content = p_soup.find_all('div',{'class':'list-cont'})
-    pageCar = []
+    p_n=p_r.text
+    p_soup=BeautifulSoup(p_n,'html.parser')
+    p_content = p_soup.find_all('div',{'class':'l_bar'})
+    pageNovel= []
 
-    for car in p_content:
-        carDic = {}
-        carDic['picUrl'] = car.find('div',{'class':'list-cont-img'}).find('img')['src']
-        carDic['name'] = car.find('div',{'class':'list-cont-main'}).find('a').text
-        try:
-            carDic['score'] = car.find('span',{'class':'score-number'}).text
-        except Exception as e:
-            carDic['score'] = ''
+    for novel in p_content:
+        novelDic = {}
+        novelDic['picUrl'] = novel.find('div',{'class':'l_pic'}).find('img')['src']
+        novelDic['name'] = novel.find('div',{'class':'l_rc'}).find('a').text        
         
-        pageCar.append(carDic)
-    return pageCar  
+        pageNovel.append(novelDic)
+    return pageNovel  
 
 pool = mp.Pool()
 multi_res = [pool.apply_async(crawl_page,(url,)) for url in urls]
-pageCars = [res.get() for res in multi_res]
+pageNovels = [res.get() for res in multi_res]
 
-for pageCar in pageCars:
-    for car in pageCar:
-        cars.append(car)
-print(len(cars))
+for pageNovel in pageNovels:
+    for novel in pageNovel:
+        novels.append(novel)
+print(len(novels))
 t2= time.time()
 print(t2-t1)
